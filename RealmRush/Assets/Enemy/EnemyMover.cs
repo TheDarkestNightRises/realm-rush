9c2105a24956d8f8d95769f2cb8c8337b23761a8
TheDarkestNightRises <91905169+TheDarkestNightRises@users.noreply.github.com>
2022-03-10 11:45:42 +0100

    Set box text and builded grid system

---
+++ RealmRush/Assets/Enemy/EnemyMover.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMover : MonoBehaviour
+{
+
+    [SerializeField] List<Waypoint> path = new List<Waypoint>();
+    [SerializeField] float waitTime = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(FollowPath()); 
+    }
+
+    private IEnumerator FollowPath()
+    {
+        foreach(Waypoint waypoint in path)
+        {
+            transform.position = waypoint.transform.position;
+            yield return new WaitForSeconds(waitTime);
+        }
+    }
+}
